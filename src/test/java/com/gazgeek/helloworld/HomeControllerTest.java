8535425965081881c0a98689d734e5d79fab1845
erdenayates <devopserd@gmail.com>
2021-04-02 02:32:44 +0300

    updated again

--- src/test/java/com/gazgeek/helloworld/HomeControllerTest.java
+++ src/test/java/com/gazgeek/helloworld/HomeControllerTest.java
@@ -28,7 +28,7 @@ public class HomeControllerTest {
     public void helloWorld() {
         getRequest("/")
             .assertStatusCode(OK)
-            .assertResponseBody("Hello from bestcloudforme! And greetings from Erdenay!");
+            .assertResponseBody("Hello from bestcloudforme! And greetings from Erdenayx!");
     }
 
     private HelloWorldResponse getRequest(String uri) {
