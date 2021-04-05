e952097b29a6c50c498e015f458eac1ad8f52bd9
erdenayates <71449664+erdenayates@users.noreply.github.com>
2021-04-05 13:29:08 +0300

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -2,5 +2,5 @@ docker rmi -f erdates/bestcloudforme
 docker build -f Dockerfile -t erdates/bestcloudforme .
 docker rm -f hello-bestcloudforme
 docker run --name hello-bestcloudforme -d -p 80:8080 erdates/bestcloudforme
-docker login -u erdates -p bestcloudforme
+docker login -u erdates -p $PASSWORD
 docker push erdates/bestcloudforme
