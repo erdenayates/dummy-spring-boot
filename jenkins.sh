2f942dcc26120a7dbdc7487d21f4e68088783a61
erdenayates <71449664+erdenayates@users.noreply.github.com>
2021-04-05 13:28:03 +0300

    Add files via upload

---
+++ jenkins.sh
@@ -0,0 +1,6 @@
+docker rmi -f erdates/bestcloudforme
+docker build -f Dockerfile -t erdates/bestcloudforme .
+docker rm -f hello-bestcloudforme
+docker run --name hello-bestcloudforme -d -p 80:8080 erdates/bestcloudforme
+docker login -u erdates -p bestcloudforme
+docker push erdates/bestcloudforme
