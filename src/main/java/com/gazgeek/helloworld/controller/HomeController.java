3903624cf1efdb01cb2db0dd41b42e6f546f6900
erdenayates <devopserd@gmail.com>
2021-04-05 22:18:39 +0300

    demo

--- src/main/java/com/gazgeek/helloworld/controller/HomeController.java
+++ src/main/java/com/gazgeek/helloworld/controller/HomeController.java
@@ -8,7 +8,7 @@ public class HomeController {
 
     @RequestMapping("/")
     String home() {
-        return "Hello from bestcloudforme! Greetings from Silivri!";
+        return "Hello from bestcloudforme!";
     }
 
 }
